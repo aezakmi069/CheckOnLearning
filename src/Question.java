96620e63b5d7ae9ee606921c9e6c1d8c4e132ac5
Mona <maharjan1235@gmail.com>
2018-09-22 18:27:26 -0400

    Created question class w/ set get

--- src/Question.java
+++ src/Question.java
@@ -1,5 +1,38 @@
-package CheckOnLearning.src;
+package src;
 
 public class Question {
+	//instance variables
+	private String question ; // a question statement from the user
+	private String[] ans = new String[5]; // an array of answers
+	
+
+	/****************************************************** 
+	**   constructs a question with a question statement,** 
+	**   an array of answers                             **
+	*******************************************************/
+
+	public Question(String question, String[] ans) {
+
+		this.question = this.setQuestion(question) ;
+		this.ans = this.setAns(ans) ;
+		
+	}
+
+	// get methods
+	public String getQuestion(){
+		return Question;
+	}
+
+	public String[] ans(){
+		return ans;
+	}
+	// set methods
+	public void setQuestion(String question){
+		this.question = question;
+	}
+
+	public void setAns(String[] ans){
+		this.ans = ans;
+	}
 	
 }
