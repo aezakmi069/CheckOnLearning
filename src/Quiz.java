1778f43aa8ac84022f289351f13c2ec408dcccbe
Pradip Khanal <pkhanal@Pradips-MBP.the-beach.net>
2018-09-23 19:22:10 -0400

    Added some of start()

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,28 +1,57 @@
 package src;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.*;
+import java.io*;
 
 public class Quiz{
-	private ArrayList<Question> qr = new ArrayList<Question>();
+	private ArrayList<Question> q= new ArrayList<Question>();
+	private int correct = 0;
 
 	//Create a Quiz 
-	public Quiz(ArrayList<Question>){
-		this.qr = qr;
+	public Quiz(ArrayList<Question> q){
+		this.setQ();
 
 	}
 
 	// Get methods
-	public ArrayList<Question> qr(){
-		Iterator it = qr.iterator();
-		while (it.hasNext()){
-			System.out.println(it.next());
+	public ArrayList<Question> getQ(){
+		return q;
+	}
+
+	public int getCorrect() {
+		return this.correct;
+	}
+
+	// Set methods
+	public void setQ (ArrayList<Question> q){
+		this.q = q;
+	}
+
+	public void setCorrect (int num) {
+		this.corect = num;
+	}
+
+
+	/** Starts a quiz
+	*** Increments the correct variable if Corrct	
+	**  If incorrect, displays the correct answer
+	*/
+	public void start() {
+		ListIterator<Question> iterator = this.getQ().listIterator();
+
+		System.out.print("\n\n=======|| Welcome to THE QUIZ ||========\n\n");
+
+		// Take the quiz
+		while (iterator.hasNext()) {
+			Question x = iterator.next();
+
+			// Print the question statement along with answer choices
+			System.out.print(x.getQuestion() + "\n");
+			String[] ans = x.getAns();
+			
 		}
-
-		return qr;
 	}
 
-	public void setqr(ArrayList<Question>){
-		this.qr = qr;
-	}
 
 }
